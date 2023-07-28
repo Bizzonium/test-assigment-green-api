d8c41ae5ca1d7c0fbc7780b275946ab65cce00d5
Bizzonium <azizonkg@gmail.com>
2023-07-29 00:36:25 +0600

    Feat(api): add request logging middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { LogLevel } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { PORT } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const isDev: boolean = process.env.NODE_ENV == 'development';
+
+  const logger: LogLevel[] = [];
+  if (isDev) {
+    logger.push('log', 'error', 'warn', 'verbose', 'debug');
+  } else {
+    logger.push('log', 'error', 'warn');
+  }
+
+  const app = await NestFactory.create(AppModule, { logger });
   await app.listen(PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
