6f7a1b1d0f55b68236b086e1ba7e58c9f39404d9
Bizzonium <azizonkg@gmail.com>
2023-07-29 18:40:02 +0600

    Feat: use configService's value parser in config

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,4 +1,4 @@
 import { configService } from 'nest-shared';
 
 export const PORT: number = configService.getPort() || 3000;
-export const HOST: string = process.env.HOST || '0.0.0.0';
+export const HOST: string = configService.getValue<string>('HOST', false) || '0.0.0.0';
