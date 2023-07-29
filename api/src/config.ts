2d7f31ad94c1ca9e1c96414c9db0dc48dcf21e86
Bizzonium <azizonkg@gmail.com>
2023-07-30 02:37:47 +0600

    Feat(api,processor): improve port config default behaviour

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,6 +1,6 @@
 import { configService } from 'nest-shared';
 
-export const PORT: number = configService.getPort() || 3000;
+export const PORT: number = configService.getValue<number>('PORT', false) || configService.getPort() || 3000;
 export const HOST: string = configService.getValue<string>('HOST', false) || '0.0.0.0';
 export const AMQP_URL: string = configService.getValue<string>('AMQP_URL', false) || 'amqp://localhost:5672';
 export const AMQP_QUEUE: string = configService.getValue<string>('AMQP_QUEUE', false) || 'example_queue';
