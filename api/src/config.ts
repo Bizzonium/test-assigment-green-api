081be176892186bdd19417d178490856e1a8d2c8
Bizzonium <azizonkg@gmail.com>
2023-07-30 02:32:32 +0600

    Feat(api,processor): make env variables optional

--- api/src/config.ts
+++ api/src/config.ts
@@ -2,5 +2,5 @@ import { configService } from 'nest-shared';
 
 export const PORT: number = configService.getPort() || 3000;
 export const HOST: string = configService.getValue<string>('HOST', false) || '0.0.0.0';
-export const AMQP_URL: string = configService.getValue<string>('AMQP_URL', true);
-export const AMQP_QUEUE: string = configService.getValue<string>('AMQP_QUEUE', false);
+export const AMQP_URL: string = configService.getValue<string>('AMQP_URL', false) || 'amqp://localhost:5672';
+export const AMQP_QUEUE: string = configService.getValue<string>('AMQP_QUEUE', false) || 'example_queue';
